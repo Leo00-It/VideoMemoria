bd466d9db863997d198aa544f8c398ab97a3ef43
Leo00-It <leo.mich008@gmail.com>
2025-03-20 15:32:57 -0600

    Configurando GitHub Pages correctamente

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/VideoMemoria/",  // Asegúrate de cambiarlo por el nombre de tu repo en GitHub
   plugins: [react()],
-})
+});
