288a486ead84e924088ff54ea1cda6ce1bc2ac3e
Leo00-It <leo.mich008@gmail.com>
2025-03-20 15:37:30 -0600

    Corrigiendo configuración de GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/VideoMemoria/",  // Asegúrate de cambiarlo por el nombre de tu repo en GitHub
+  base: "/VideoMemoria/", // Cambia "VideoMemoria" por el nombre EXACTO de tu repositorio
   plugins: [react()],
 });
+
